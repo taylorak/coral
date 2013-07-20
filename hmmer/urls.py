c93ea290e09c41743c404f36d5bf70e11a1b0bc3
Taylor <taylor@carotte.ics.hawaii.edu>
2013-07-19 15:06:58 -1000

    finished downloads

--- hmmer/urls.py
+++ hmmer/urls.py
@@ -5,7 +5,8 @@ Created on Jul 8, 2013
 '''
 from django.conf.urls import patterns, url
 urlpatterns = patterns('',
-    url(r'^$', 'hmmer.views.InputFormDisplay'),
+    url(r'^$', 'hmmer.views.inputFormDisplay'),
     url(r'^status/(?P<id>\d+\.\d+\.\d+)/$','hmmer.views.status'),
     url(r'^status/(?P<id>\d+\.\d+\.\d+)/(?P<site>[A-Za-z]+\d+)/$','hmmer.views.chart'),
+    url(r'^status/(?P<id>\d+\.\d+\.\d+)/download/(?P<filename>.+)/$','hmmer.views.download'),
 )
--- hmmer/views.py
+++ hmmer/views.py
@@ -1,17 +1,19 @@
 # Create your views here.
 #from django.contrib.auth.decorators import login_required
-from django.template import RequestContext
-from django.http import HttpResponseRedirect
+from django.template import RequestContext 
+from django.http import HttpResponseRedirect,HttpResponse
 from django.shortcuts import render_to_response
 from celery.result import AsyncResult
 from models import InputForm,symTyperTask 
 from tasks import handleForm
 from django.conf import settings
+from django.utils.encoding import smart_str
 import os
 import time
 import csv
+import urllib
 
-###
+### MOVE LATER
 def writeFile(origin,destination):
     with open(destination,'w') as dest:
         for chunk in origin.chunks():
@@ -30,9 +32,65 @@ def searchTable(tablePath,site):
                 values = row2[1:]
                 return dict(zip(keys, values))
     return False
+
+def listTable(tablePath):
+    table = []
+    with open(tablePath,'rb') as tsv:
+        reader = csv.reader(tsv)
+    
+        for row in reader:         
+            row2 = row[0].split()
+            table.append(table)
+    return table
+
+def servFile(request, ready, filename, fPath, fsize):
+    response = HttpResponse(mimetype='application/force-download')
+    if(not Status.SUCCESS):
+        return response
+    #response['Content-Type'] = contentType
+    response['Content-Disposition'] = 'attachment; filename=%s' %(smart_str( filename  ) ) 
+    response['X-Sendfile'] = urllib.quote(fPath)
+    response['Content-Transfer-Encoding'] = "binary"
+    response['Expires'] = 0
+    response['Accept-Ranges'] = 'bytes'
+    response['Cache-Control'] = "private"
+    response['Pragma'] = 'private'
+    httprange = request.META.get("HTTP_RANGE", None)
+    if(httprange):
+        rng = httprange.split("=")
+        cnt = rng[-1].split("-")
+        response['Content-Length'] = fsize - int(cnt[0])
+        response['Content-Range'] = str(httprange) + str(response['Content-Length']) + "/" + str(fsize)
+    else:
+        response['Content-Length'] = fsize
+    with open(fPath) as outfile:
+        buf = outfile.read(4096)
+        while len(buf) == 4096:
+            response.write(buf)
+            buf = outfile.read(4096)
+        if(len(buf) != 0):
+            response.write(buf) 
+    return response
+
+
+class Status:
+    SUCCESS = 1
+    FAILURE = 2 
+    PENDING = 3
+
+def taskReady(task):
+    if task.ready():
+        if task.successful():
+            return Status.SUCCESS
+        else:
+            return Status.FAILURE
+    else:
+        return Status.PENDING
+
+
 ###
 
-def InputFormDisplay(request):
+def inputFormDisplay(request):
     if request.method == 'POST':
         form = InputForm(request.POST, request.FILES)
         if form.is_valid():
@@ -49,16 +107,6 @@ def InputFormDisplay(request):
             writeFile(request.FILES['fasta_File'],os.path.join(dataDir,"input.fasta"))
             writeFile(request.FILES['sample_File'],os.path.join(dataDir,"input.ids"))
 
-# change later
-#            with open(os.path.join(dataDir,"input.fasta"),'w') as dest:
-#                for chunk in request.FILES['fasta_File'].chunks():
-#                    dest.write(chunk)
- 
-#            with open(os.path.join(dataDir,"input.ids"),'w') as dest:
-#                for chunk in request.FILES['sample_File'].chunks():
-#                    dest.write(chunk)
-                     
-
             task = handleForm.delay("data/input.fasta","data/input.ids",parentDir)
 
             sym_task.celeryUID = task.id
@@ -71,8 +119,23 @@ def InputFormDisplay(request):
 
 def status(request, id):
     sym_task = symTyperTask.objects.get(UID = id)
-    result = AsyncResult(sym_task.celeryUID)
+    task = AsyncResult(sym_task.celeryUID)
+
     dirs = headers = False
+    downloads = ['ALL_counts.tsv','DETAILED_counts.tsv']
+    
+    ready = taskReady(task)
+    if ready == Status.SUCCESS:
+        message = "success!"
+        dirs = os.walk(os.path.join(settings.SYMTYPER_HOME,str(id) + "/data/hmmer_parsedOutput/")).next()[1]
+    elif ready == Status.FAILURE:
+        message = "failure!"
+    else:
+        message = "pending..."
+
+    return render_to_response('status.html',RequestContext(request, {'message':message,'dirs':dirs,'downloads':downloads,'id':id}))
+
+"""
     if result.ready():
         if result.successful():
             message = "success!"
@@ -81,26 +144,21 @@ def status(request, id):
             message = "failure!"
     else:
         message = "pending..."
-    return render_to_response('status.html',RequestContext(request, {'message':message,'dirs':dirs,'id':id}))
+"""
         
 def chart(request,id,site):
     path = os.path.join(settings.SYMTYPER_HOME,str(id) + "/data/hmmer_parsedOutput/")
-    dictionary = searchTable(os.path.join(path,'ALL_counts.tsv'),site)
+    ALL_dict = searchTable(os.path.join(path,'ALL_counts.tsv'),site)
+    DETAILED_dict = searchTable(os.path.join(path,'DETAILED_counts.tsv'),site)
+    return render_to_response('chart.html',RequestContext(request, {'ALL_dict':ALL_dict,'DETAILED_dict':DETAILED_dict}))
 
-# fix this
-#    dictionary = False
-#    with open(os.path.join(path,'ALL_counts.tsv'),'rb') as tsv:
-#        reader = csv.reader(tsv)
-#
-#        headers = reader.next()
-#        keys = [header.split('_')[0] for header in headers[0].split()[1:]]
-#
-#        for row in reader:
-#            row2 = row[0].split()
-#            if  row2[0] == str(site):
-#                values = row2[1:]
-#                dictionary = dict(zip(keys, values))
+def download(request, id, filename):
+    sym_task = symTyperTask.objects.get(UID=id)
+    task = AsyncResult(sym_task.celeryUID)
+    ready = taskReady(task)
 
-    return render_to_response('chart.html',RequestContext(request, {'dictionary':dictionary}))
+    fPath = os.path.join(settings.SYMTYPER_HOME,str(id) + "/data/hmmer_parsedOutput/" + str(filename))
+    fsize = os.stat(fPath).st_size
+    return servFile(request, ready, filename, fPath, fsize)
 
 
