c37d2bfc26311a5577c8b94ac999b17f3a6d7b4d
taylorak <taykenned@gmail.com>
2013-11-06 15:17:33 -1000

    added new task

--- hmmer/tasks.py
+++ hmmer/tasks.py
@@ -15,10 +15,12 @@ def handleForm(fasta, sample, uid):
     parentDir = os.path.join(settings.SYMTYPER_HOME, uid)
     #print parentDir
     imageDir = os.path.join(settings.PROJECT_ROOT, 'static', 'img', uid)
-    print imageDir
+    #print imageDir
 
+    #with open(os.path.join(parentDir, 'log')) as log:
     os.chdir(parentDir)
     os.system("""python /home/taylor/symTyper/symTyper.py --threads 12 clade --i %s -s %s""" % (fasta, sample))
+#    log.write('finished clades/n')
     os.system("""python /home/taylor/symTyper/symTyper.py  -t 3 subtype -H data/hmmer_hits/ -s %s -b data/blast_output/ -r data/blastResults/ -f data/fasta"""%(sample))
     os.system("""python /home/taylor/symTyper/symTyper.py  -t 3 resolveMultipleHits -s %s -m data/blastResults/MULTIPLE/fasta/ -c data/resolveMultiples/"""%(sample))
     os.system("""python /home/taylor/symTyper/symTyper.py  -t 3 builPlacementTree -c data/resolveMultiples/correctedMultiplesHits/corrected -n /home/taylor/Clade_Trees/ -o data/placementInfo""")
